66fb04fe53ea47ea42aa2b1cea7601e02333b291
Ruddy Gobillard <ruddy.gobillard@etudiant.univ-reims.fr>
2026-03-16 17:33:13 +0100

    Feat: lint html & script

--- scriptTableau.js
+++ scriptTableau.js
@@ -4,6 +4,7 @@ function roundTwoDigit(number) {
 
 const tableSection = document.getElementById("tableSection");
 const startButton = document.getElementById("startButton");
+
 startButton.addEventListener("click", () => {
   // Fonction qui se lance quand l'utilisateur appuie sur le bouton
   // Lancement de la simulation
@@ -22,9 +23,9 @@ startButton.addEventListener("click", () => {
     mensuel: 'Mois',
     semestre: 'Semestres'
   };
+
   const typeDelta = periodiciteMap[periodiciteRemboursement];
   let dividerDelta = 1;
-
 
   switch (typeDelta) {
     case 'Trimestres':
@@ -41,24 +42,26 @@ startButton.addEventListener("click", () => {
       break;
   }
 
-  tauxInteret = tauxInteret**(1/dividerDelta) - 1;
-  console.log(tauxInteret)
+  tauxInteret = tauxInteret ** (1 / dividerDelta) - 1;
+  console.log(tauxInteret);
 
   // mise en place tableau
-  table.innerHTML = `<thead>
-            <tr>
-                <th>${typeDelta}</th>
-                <th>Capital restant du en début de période</th>
-                <th>Intérêts</th>
-                <th>Amortissements</th>
-                <th>${periodiciteRemboursement}</th>
-                <th>Capital restant du en fin de période</th>
-            </tr>
-        </thead>`;
+  table.innerHTML = `
+    <thead>
+      <tr>
+        <th>${typeDelta}</th>
+        <th>Capital restant du en début de période</th>
+        <th>Intérêts</th>
+        <th>Amortissements</th>
+        <th>${periodiciteRemboursement}</th>
+        <th>Capital restant du en fin de période</th>
+      </tr>
+    </thead>`;
 
   let capitalFin = montantEmprunt;
-  const annuiteEmprunt = roundTwoDigit(montantEmprunt * (tauxInteret / (1 - (1 + tauxInteret)**(-duration))));
+  const annuiteEmprunt = roundTwoDigit(montantEmprunt * (tauxInteret / (1 - (1 + tauxInteret) ** (-duration))));
   let sumInteret = 0;
+
   for (let delta = 1; delta <= duration; delta++) {
     // un seul cycle, effectuée à chaque tour
     let capitalDeb = roundTwoDigit(capitalFin);
@@ -73,29 +76,31 @@ startButton.addEventListener("click", () => {
     sumInteret += parseFloat(interetPeriode);
 
     // ajout 1 seule ligne
-    table.innerHTML += `<tbody>
-                <tr>
-                    <th>${delta}</th>
-                    <td>${capitalDeb}</td>
-                    <td>${interetPeriode}</td>
-                    <td>${amortissementCapital}</td>
-                    <td>${annuiteEmprunt}</td>
-                    <td>${capitalFin}</td>
-                </tr>
-            </tbody>`;
+    table.innerHTML += `
+      <tbody>
+        <tr>
+          <th>${delta}</th>
+          <td>${capitalDeb}</td>
+          <td>${interetPeriode}</td>
+          <td>${amortissementCapital}</td>
+          <td>${annuiteEmprunt}</td>
+          <td>${capitalFin}</td>
+        </tr>
+      </tbody>`;
   }
 
   // affichage ligne total
-  table.innerHTML += `<tbody>
-                <tr>
-                    <th>Total</th>
-                    <td>-</td>
-                    <td>${roundTwoDigit(sumInteret)}</td>
-                    <td>${montantEmprunt}</td>
-                    <td>${annuiteEmprunt*duration}</td>
-                    <td>-</td>
-                </tr>
-            </tbody>`;
+  table.innerHTML += `
+    <tbody>
+      <tr>
+        <th>Total</th>
+        <td>-</td>
+        <td>${roundTwoDigit(sumInteret)}</td>
+        <td>${montantEmprunt}</td>
+        <td>${annuiteEmprunt * duration}</td>
+        <td>-</td>
+      </tr>
+    </tbody>`;
 
   document.body.append(table);
 });
